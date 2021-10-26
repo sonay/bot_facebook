6e86521353fefd8fd250e7d4ee69b42ba8cb15ed
sonay <twentysevenhorses>
2021-10-26 13:27:45 +0300

    Accumulate stats and overwrite if summary file exists

--- src/summary.py
+++ src/summary.py
@@ -30,11 +30,32 @@ def main():
 def create_summary(csv_dir):
     summary_csv_path = csv_dir / SUMMARY_CSV_PATH
     write_headers(summary_csv_path)
+    for file in csv_dir.iterdir():
+        if file.match("*.csv") and file != summary_csv_path:
+            url_md5 = file.name[len("bot_facebook_"):-4]
+            summary = summarize(file)
+            save_summary(summary_csv_path, [url_md5, *summary])
 
 
 def write_headers(csv_file):
-    with csv_file.open(newline='', mode="x", encoding="utf-8") as f:
+    with csv_file.open(newline='', mode="w", encoding="utf-8") as f:
         csv.writer(f).writerow(SUMMARY_HEADERS)
+
+
+def summarize(file):
+    summary = [0, 0, 0]
+    with file.open(newline='', mode="r", encoding="utf-8") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            summary[0] += int(row[0])  # likes
+            summary[1] += int(row[1])  # comments
+            summary[2] += int(row[2])  # shares
+    return tuple(summary)
+
+
+def save_summary(csv_file, row):
+    with csv_file.open(newline='',  mode="a+", encoding="utf-8") as f:
+        csv.writer(f).writerow(row)
 
 
 if __name__ == '__main__':
