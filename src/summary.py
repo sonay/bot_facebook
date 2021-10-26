ca3208e7519f937f86ae4ee18c7bbeb32ef599a4
sonay <twentysevenhorses>
2021-10-26 13:17:20 +0300

    Add argument parsing and checks

--- src/summary.py
+++ src/summary.py
@@ -1,9 +1,27 @@
+import argparse
 import logging
 import sys
 
 _logger = logging.getLogger(__name__)
 
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dir",
+                        help="Enter the csv source directory, either OCR or DOM")
+    args = parser.parse_args()
+
+    try:
+        csv_dir = args.dir
+        if csv_dir not in ["OCR", "DOM"]:
+            raise ValueError()
+    except ValueError:
+        _logger.critical("Missing or illegal --dir parameter, use with -h for help.")
+        return
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s [%(levelname)s]: %(message)s',
                         stream=sys.stdout)
+    main()
