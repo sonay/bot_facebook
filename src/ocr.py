25bf0e150701fb4bfd89c9c3aa6aaef449cde56f
sonay <twentysevenhorses>
2021-10-26 11:17:38 +0300

    Save tesseract output to corresponding .txt files.

--- src/ocr.py
+++ src/ocr.py
@@ -1,7 +1,25 @@
 import logging
 import sys
+from pathlib import Path
+
+from PIL import Image
+from pytesseract import pytesseract
+
+OCR_DIR = Path("OCR")
+
+
+def main():
+    for file in OCR_DIR.iterdir():
+        if file.match("*.png"):
+            tes_text = pytesseract.image_to_string(
+                Image.open(file, formats=("PNG",)), config='--psm 6', lang="tur")
+
+            with open(file.with_suffix(".txt"), "w", encoding="utf-8") as txt:
+                txt.write(tes_text)
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s [%(levelname)s]: %(message)s',
                         stream=sys.stdout)
+    main()
